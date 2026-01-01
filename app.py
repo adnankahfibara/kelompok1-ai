81412b3a51c8dafb91c9a271fd369e5baa9fb21f
adnankahfibara <adnanxkb@gmail.com>
2026-01-02 03:20:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        # proses input form dan hasil diagnosa
+        return render_template("index.html", hasil=hasil)
+    return render_template("index.html")
